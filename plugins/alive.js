b4fb9cf2c43e5821f94dd74b303283b23dd984d5
Hashanmddevice <Hashanmdofc@gmail.com>
2024-12-07 11:20:54 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -10,7 +10,7 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
     
-await conn.sendMessage(from,{image: {url: `https://i.ibb.co/jyVLMdD/infinitywabot.jpg`},caption: `Hello ${pushname}, hello iam created by Hashan Sathsara🌝❤️‍🔥 !`},{quoted: mek})
+await conn.sendMessage(from,{image: {url: `https://files.catbox.moe/fbebro.jpeg`},caption: `Hello ${pushname}, hello iam created by Hashan Sathsara🌝❤️‍🔥 ! This Bot Created Dark Hackers Ofc team members !`},{quoted: mek})
     
 }catch(e){
 console.log(e)
